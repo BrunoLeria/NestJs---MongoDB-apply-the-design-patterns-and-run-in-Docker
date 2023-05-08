c0572420fbd97c67fbb33fbed7a599886b8e3b00
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-07 22:24:10 -0300

    Add Config, TypeORM, User, and Auth modules to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ormConfig } from './database/config/ormconfig';
+import { UserModule } from './components/user/user.module';
+import { AuthModule } from './components/auth/auth.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot(ormConfig()),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
