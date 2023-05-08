13591e00211327e30c3bbbe7981f061dd5a65cc5
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-07 22:32:31 -0300

    Add user creation functionality to UserController and UserService

--- src/components/user/user.controller.ts
+++ src/components/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { User } from './entity/user.entity';
+import { UserService } from './user.service';
 
-@Controller('user')
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  public async create(@Body() userDto: any): Promise<User> {
+    return await this.userService.create(userDto);
+  }
+}
