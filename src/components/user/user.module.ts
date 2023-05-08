38b19b3cd6e8f14dbba19b4bdb5498e1ba6368b6
Bruno Leria <brunovolpatol@hotmail.com>
2023-05-07 22:55:04 -0300

    Refactor user module to use interfaces for UserService and UserRepository

--- src/components/user/user.module.ts
+++ src/components/user/user.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entity/user.entity';
+import { UserRepository } from '../../repositories/user.repository';
+import { UserService } from './user.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [
+    {
+      provide: 'UserRepositoryInterface',
+      useClass: UserRepository,
+    },
+    {
+      provide: 'UserServiceInterface',
+      useClass: UserService,
+    },
+  ],
 })
 export class UserModule {}
